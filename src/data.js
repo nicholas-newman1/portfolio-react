9f1594a1c796ddb84c0962b44c725dc03a475720
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-11 07:13:48 -0500

    updated project data

--- src/data.js
+++ src/data.js
@@ -1,28 +1,20 @@
 import stockSitePreview from './images/stock-site/stock-site-home-page-preview.jpg';
-import stockSite1 from './images/stock-site/stock-site-home-page.png';
-import stockSite2 from './images/stock-site/stock-site-djia-quote.png';
-import stockSite3 from './images/stock-site/stock-site-search-results-page.png';
-import stockSite4 from './images/stock-site/stock-site-cryptocurrencies-page.png';
+import stockSite from './images/stock-site/stock-site-home-page.png';
 import fakeStorePreview from './images/fake-store/fake-store-home-page-preview.jpg';
-import fakeStore1 from './images/fake-store/fake-store-home-page.png';
-import fakeStore2 from './images/fake-store/fake-store-product-page.png';
-import ticTacToe1 from './images/tic-tac-toe/tic-tac-toe-new-game-menu.png';
-import ticTacToe2 from './images/tic-tac-toe/tic-tac-toe-mid-game.png';
+import fakeStore from './images/fake-store/fake-store-home-page.png';
+import ticTacToePreview from './images/tic-tac-toe/tic-tac-toe-mid-game.png';
+import ticTacToe from './images/tic-tac-toe/tic-tac-toe-mid-game.png';
+import watchShopPreview from './images/watch-shop/watch-shop-home-page-preview.jpg';
+import watchShop from './images/watch-shop/watch-shop-home-page.png';
+import todoAppPreview from './images/todo-app/todo-app.png';
+import todoApp from './images/todo-app/todo-app.png';
 
 export const projects = [
   {
     id: 1,
     title: 'Stock Site',
-    preview: {
-      src: stockSitePreview,
-      alt: 'Stock site home page',
-    },
-    images: [
-      { src: stockSite1, alt: 'Stock site home page' },
-      { src: stockSite2, alt: 'Stock site dow jones industrial average quote' },
-      { src: stockSite3, alt: 'Stock site search results page' },
-      { src: stockSite4, alt: 'Stock site cryptocurrencies page' },
-    ],
+    preview: { src: stockSitePreview, alt: 'Stock site home page' },
+    fullImage: { src: stockSite, alt: 'Stock site home page' },
     tags: [
       { text: 'html', backgroundColor: '#E54D26' },
       { text: 'css', backgroundColor: '#0071BD' },
@@ -48,14 +40,8 @@ export const projects = [
   {
     id: 2,
     title: 'Fake Store',
-    preview: {
-      src: fakeStorePreview,
-      alt: 'fake store home page',
-    },
-    images: [
-      { src: fakeStore1, alt: 'Fake store home page' },
-      { src: fakeStore2, alt: 'Fake store product page' },
-    ],
+    preview: { src: fakeStorePreview, alt: 'fake store home page' },
+    fullImage: { src: fakeStore, alt: 'Fake store home page' },
     tags: [
       { text: 'html', backgroundColor: '#E54D26' },
       { text: 'css', backgroundColor: '#0071BD' },
@@ -76,11 +62,8 @@ export const projects = [
   {
     id: 3,
     title: 'Tic Tac Toe',
-    preview: { src: ticTacToe1, alt: 'Tic tac toe new game menu' },
-    images: [
-      { src: ticTacToe1, alt: 'Tic tac toe new game menu' },
-      { src: ticTacToe2, alt: 'Tic tac toe mid game' },
-    ],
+    preview: { src: ticTacToePreview, alt: 'Tic tac toe mid game' },
+    fullImage: { src: ticTacToe, alt: 'Tic tac toe mid game' },
     tags: [
       { text: 'html', backgroundColor: '#E54D26' },
       { text: 'scss', backgroundColor: '#C6538C' },
@@ -102,5 +85,35 @@ export const projects = [
     ],
     liveUrl: 'https://stupefied-franklin-2a03b1.netlify.app',
     codeUrl: 'https://github.com/nicholas-newman1/tic-tac-toe',
+  },
+  {
+    id: 4,
+    title: 'Watch Shop',
+    preview: { src: watchShopPreview, alt: 'Watch shop home page' },
+    fullImage: { src: watchShop, alt: 'watch shop home page' },
+    tags: [
+      { text: 'html', backgroundColor: '#E54D26' },
+      { text: 'scss', backgroundColor: '#C6538C' },
+      { text: 'js', backgroundColor: '#E7A229' },
+    ],
+    features: ['Browse products'],
+    skills: ['SCSS Preprocessor', 'BEM naming convention', 'Git Workflow'],
+    liveUrl: 'https://boring-wing-3a4e41.netlify.app',
+    codeUrl: 'https://github.com/nicholas-newman1/watch-shop',
+  },
+  {
+    id: 5,
+    title: 'Todo App',
+    preview: { src: todoAppPreview, alt: 'Todo app' },
+    fullImage: { src: todoApp, alt: 'Todo app' },
+    tags: [
+      { text: 'html', backgroundColor: '#E54D26' },
+      { text: 'scss', backgroundColor: '#C6538C' },
+      { text: 'js', backgroundColor: '#E7A229' },
+    ],
+    features: ['Browse products'],
+    skills: ['SCSS Preprocessor', 'BEM naming convention', 'Git Workflow'],
+    liveUrl: 'https://gallant-meninsky-30e418.netlify.app',
+    codeUrl: 'https://github.com/nicholas-newman1/todo-app-vanilla',
   },
 ];
