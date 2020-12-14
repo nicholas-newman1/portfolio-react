f132b36e21c41573f48086074ee2dcae8630968f
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-14 16:18:42 -0500

    added contact details

--- src/pages/HomePage/Contact/Contact.js
+++ src/pages/HomePage/Contact/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Email from '../../../components/svgs/Email';
+import Phone from '../../../components/svgs/Phone';
 import './contact.css';
 
 const Contact = () => {
   return (
     <section id='contact' className='contact container'>
       <h2 className='section-heading'>Contact</h2>
+
       <form className='contact__form'>
         <label className='contact__label'>
           Name:
@@ -20,6 +23,21 @@ const Contact = () => {
         </label>
         <button className='contact__submit'>Send</button>
       </form>
+
+      <div className='contact__details'>
+        <a className='contact__link' href='mailto:nicholas.newman1@hotmail.com'>
+          <div className='contact__icon'>
+            <Email />
+          </div>
+          nicholas.newman1@hotmail.com
+        </a>
+        <a className='contact__link' href='tel:6479928126'>
+          <div className='contact__icon'>
+            <Phone />
+          </div>
+          (647)-992-8126
+        </a>
+      </div>
     </section>
   );
 };
